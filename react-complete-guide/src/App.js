ebecad095c4c24ec136daf52dea8f1cfeed841d7
dfilter <dfilter@evertz.com>
2020-08-12 14:50:14 -0400

    38. Understanding & Using State

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,15 +7,24 @@ class App extends Component {
   /**
    * App is the root component which all other components will be nested inside of
    */
+  state = {
+    persons: [
+      { name: 'John', age: 28 },
+      { name: 'Jane', age: 29 },
+      { name: 'Jake', age: 26 }
+    ]
+  }
+  
   render() {
     return (
       // since "class" is a reserved name in javascript react cannot use class as an attribute name for styling classes
       <div className="App">
         <h1>Hello from react app!</h1>
         <p>It's working!</p>
-        <Person name="John" age="28" />
-        <Person name="Jane" age="29">My Hobbies: Racing</Person>
-        <Person name="Jake" age="26" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing</Person>
       </div>
     );
     // below can be used to render a component but since none is defined we can use div instead
