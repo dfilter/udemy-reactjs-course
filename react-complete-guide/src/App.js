c410153a86e3a3fd6360c642c9caee6aa55a743b
dfilter <dfilter@evertz.com>
2020-08-12 14:08:07 -0400

    30. Understanding JSX

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,11 +7,14 @@ class App extends Component {
    */
   render() {
     return (
-      // The markup below is NOT html it is jsx that ultimately compiles to js
       <div className="App">
         <h1>Hello from react app!</h1>
       </div>
     );
+    // below can be used to render a component but since none is defined we can use div instead
+    // second argument is config, all fallowing arguments are nested each one being nested inside
+    // ultimately the code above will look like the code below
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello from react app!'))
   }
 }
 
