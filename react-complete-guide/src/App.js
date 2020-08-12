62deb60be61a4078d8542fb7322ae994bb32f539
dfilter <dfilter@evertz.com>
2020-08-12 13:59:45 -0400

    29. Understanding Component Basics

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  /**
+   * App is the root component which all other components will be nested inside of
+   */
   render() {
     return (
+      // The markup below is NOT html it is jsx that ultimately compiles to js
       <div className="App">
         <h1>Hello from react app!</h1>
       </div>
--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Here we render the root "App" component
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
