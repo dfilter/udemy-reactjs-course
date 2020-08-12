693bf6bb1296f5a4f631c68f72c60af140bed3a6
dfilter <dfilter@evertz.com>
2020-08-12 14:30:23 -0400

    35. Outputting Dynamic Content

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,7 +3,8 @@ import React from 'react'
 
 // in its purest form a component is simply a function that returns jsx
 const person = () => {
-  return <p>I'm a person!</p>
+  // to execute javascript inline the javascript must be wrapped in "{}"
+  return <p>I'm a person, and am {Math.floor(Math.random() * 30)}</p>
 }
 
 export default person
