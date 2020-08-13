a6c0f1cebb4ae8747e75701daf665c48497e76bc
dfilter <dfilter@evertz.com>
2020-08-13 10:57:03 -0400

    44. Using the useState() Hook for State Manipulation

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 // components imported must have uppercase
 import Person from './Person/Person'
 
-class App extends Component {
-  /**
-   * App is the root component which all other components will be nested inside of
-   */
-  state = {
+const app = (props) => {
+  // useState() returns an array first element will always be the current state second will be a function to update the state
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: 'John', age: 28 },
       { name: 'Jane', age: 29 },
       { name: 'Jake', age: 26 }
-    ],
-    otherState: 'hello world!'
-  }
+    ]
+  })
 
-  switchNameHandler = () => {
-    // console.log('Was clicked!')
-    // this.state.persons[0].name = 'John Doe'  // don't do this!
-    // setState is a method provided by the Component class extension
-    this.setState({
+  const [ otherState, setOtherState ] = useState('hello world!')
+
+  console.log(otherState)
+
+  const switchNameHandler = () => {
+    // this does not merge with old state it replaces it
+    setPersonsState({
       persons: [
         { name: 'John Doe', age: 28 },
         { name: 'Jane', age: 29 },
@@ -29,24 +28,16 @@ class App extends Component {
     })
   }
 
-  render() {
-    return (
-      // since "class" is a reserved name in javascript react cannot use class as an attribute name for styling classes
-      <div className="App">
-        <h1>Hello from react app!</h1>
-        <p>It's working!</p>
-        {/* note that the parentheses are not used for switchNameHandler this stops the function being called as soon as it is rendered */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing</Person>
-      </div>
-    );
-    // below can be used to render a component but since none is defined we can use div instead
-    // second argument is config, all fallowing arguments are nested each one being nested inside
-    // ultimately the code above will look like the code below
-    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello from react app!'))
-  }
+  return (
+    <div className="App">
+      <h1>Hello from react app!</h1>
+      <p>It's working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My Hobbies: Racing</Person>
+    </div>
+  );
 }
 
-export default App;
+export default app;
