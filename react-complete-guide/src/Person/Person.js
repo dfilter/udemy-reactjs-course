34ec39bbdd2fbcf4612c5553ee2a3f4c1d0a8afa
dfilter <dfilter@evertz.com>
2020-08-13 11:34:21 -0400

    48. Adding Styling with Stylesheets

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,5 +1,6 @@
 // despite not invoking the React class directly we still need to import it for the component below to work
 import React from 'react'
+import './Person.css'
 
 // in its purest form a component is simply a function that returns jsx
 // props will contain all the properties/attributes that are passed to this component
@@ -7,7 +8,7 @@ import React from 'react'
 const person = (props) => {
   // to execute javascript inline the javascript must be wrapped in "{}"
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>I'm {props.name}, and am {props.age} years old.</p>
       {/* props.children is reserved to anything passed between component tags */}
       <p>{props.children}</p>
