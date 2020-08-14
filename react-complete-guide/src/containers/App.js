ef0aed07d40fb7c651e39dfd207a07c8d2a38635
dfilter <dfilter@evertz.com>
2020-08-14 15:52:46 -0400

    90. Component Creation Lifecycle in Action

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -5,6 +5,11 @@ import Cockpit from '../components/Cockpit/Cockpit'
 import Persons from '../components/Persons/Persons'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log('[App.js] constructor')
+  }
+
   state = {
     persons: [
       { id: 'ijwifghsf', name: 'John', age: 28 },
@@ -13,6 +18,20 @@ class App extends Component {
     ],
     otherState: 'hello world!',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state
+  }
+
+  // Deprecated
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount')
+  // }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount')
   }
 
   deletePersonHandler = (personIndex) => {
@@ -39,6 +58,8 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render')
+
     let persons = null
     if (this.state.showPersons) {
       persons = <Persons
