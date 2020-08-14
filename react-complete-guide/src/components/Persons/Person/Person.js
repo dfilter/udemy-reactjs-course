a1753a4a39a3ea0b6712ca9e70a3a0498582ca2c
dfilter <dfilter@evertz.com>
2020-08-14 16:18:12 -0400

    91. Component Update Lifecycle (for props Changes)

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './Person.css'
 
-const person = (props) => {
-  console.log('[Person.js] rendering...')
-  
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm {props.name}, and am {props.age} years old.</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}></input>
-    </div>
-  )
+class Person extends Component {
+
+  render() {
+    console.log('[Person.js] rendering...')
+
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm {this.props.name}, and am {this.props.age} years old.</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+      </div>
+    )
+  }
 }
 
-export default person
+export default Person
--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Person from './Person/Person'
 
-const persons = (props) => {
-  console.log('[Persons.js] rendering...')
+class Persons extends Component {
+  // initial state is undefined therefor redundant
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps', props, state)
+  //   return state
+  // }
 
-  return props.persons.map((person, index) => {
-    return <Person
-      key={person.id}
-      name={person.name}
-      age={person.age}
-      click={() => props.clicked(index)}
-      changed={(event) => props.changed(event, person.id)} />
-  })
+  // Deprecated
+  // componentWillReceiveProps(props) {
+  //   console.log('[Persons.js] componentWillReceiveProps', props)
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate', nextProps, nextState)
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate', prevProps, prevState)
+    return "Snapshot!"
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...')
+
+    return this.props.persons.map((person, index) => {
+      return <Person
+        key={person.id}
+        name={person.name}
+        age={person.age}
+        click={() => this.props.clicked(index)}
+        changed={(event) => this.props.changed(event, person.id)} />
+    })
+  }
+
+  // Deprecated
+  // componentWillUpdate() {
+  //   console.log('[Persons.js] componentWillUpdate')
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate', prevProps, prevState, snapshot)
+  }
 }
 
-export default persons
+export default Persons
