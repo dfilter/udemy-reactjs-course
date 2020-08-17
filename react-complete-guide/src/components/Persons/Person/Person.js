3bc240ba510e99391bd05b23ea681a38788d96a1
dfilter <dfilter@evertz.com>
2020-08-17 12:27:37 -0400

    109. Using PropTypes

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import Auxillary from '../../../hoc/Auxiliary'
 import withClass from  '../../../hoc/withClass'
@@ -11,6 +12,11 @@ import classes from './Person.css'
  * 2. Auxillary can be used here as well as another way to wrap adjacent jsx. This component doesn't do anything but let us 
  * have multiple jsx elements next to one another.
  * 3. Fragment which is part of react does the same thing as Auxillary
+ * 
+ * To expose the prop types needed by this component we can use the prop-types library. 
+ * We do this by defining them as an object on the propTypes attribute of the component class. Keys being the prop
+ * and value being the type within PropTypes.
+ * This feature is mainly used in development mode.
  */
 class Person extends Component {
 
@@ -23,6 +29,13 @@ class Person extends Component {
       <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}></input>
     </Auxillary>
   }
+}
+
+Person.propTypes = {
+  age: PropTypes.number,
+  changed: PropTypes.func,
+  click: PropTypes.func,
+  name: PropTypes.string
 }
 
 export default withClass(Person, classes.Person)
