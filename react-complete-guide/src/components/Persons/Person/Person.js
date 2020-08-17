423577868f161a529c21a78e1c05b0aa3631c689
dfilter <dfilter@evertz.com>
2020-08-17 11:34:16 -0400

    104. Using React.Fragment

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 
 import Auxillary from '../../../hoc/Auxiliary'
-import classes from './Person.css'
+// import classes from './Person.css'
 
 /**
- * Since in jsx render can only return one expression we have one of two choices:
+ * Since in jsx render can only return one expression we have some choices:
  * 1. If a wrapping element is not needed for structure or styling an array of jsx can be returned instead.
  * Don't forget the commas though!
  * 2. Auxillary can be used here as well as another way to wrap adjacent jsx. This component doesn't do anything but let us 
  * have multiple jsx elements next to one another.
+ * 3. Fragment which is part of react does the same thing as Auxillary
  */
 class Person extends Component {
 
