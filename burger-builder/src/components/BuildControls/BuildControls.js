4f4d3e576bfdda84838d3b6438614b18789c2615
dfilter <dfilter@evertz.com>
2020-08-17 16:52:44 -0400

    131. Adding the Build Control Component

---
+++ burger-builder/src/components/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import classes from './BuildControls.module.css'
+
+const buildControls = (props) => (
+  <div className={classes.BuildControl}></div>
+)
+
+export default buildControls
