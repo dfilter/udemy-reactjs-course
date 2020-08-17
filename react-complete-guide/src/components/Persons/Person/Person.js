fd1d232346964febff23e94a4bdead2a58bd1ff4
dfilter <dfilter@evertz.com>
2020-08-17 12:38:14 -0400

    110. Using Refs

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import Auxillary from '../../../hoc/Auxiliary'
-import withClass from  '../../../hoc/withClass'
+import withClass from '../../../hoc/withClass'
 import classes from './Person.css'
 
 /**
@@ -13,12 +13,26 @@ import classes from './Person.css'
  * have multiple jsx elements next to one another.
  * 3. Fragment which is part of react does the same thing as Auxillary
  * 
+ * 109. Using PropTypes:
  * To expose the prop types needed by this component we can use the prop-types library. 
  * We do this by defining them as an object on the propTypes attribute of the component class. Keys being the prop
  * and value being the type within PropTypes.
  * This feature is mainly used in development mode.
+ * 
+ * 110. Using Refs:
+ * If we need to reference a jsx element we can use the ref property. This can be used to select it and execute
+ * javascript code on that element. Here we give the element focus
  */
 class Person extends Component {
+  constructor(props) {
+    super(props)
+    this.inputElementRef = React.createRef()
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus()
+    this.inputElementRef.current.focus()
+  }
 
   render() {
     console.log('[Person.js] rendering...')
@@ -26,7 +40,13 @@ class Person extends Component {
     return <Auxillary>
       <p key="i1" onClick={this.props.click}>I'm {this.props.name}, and am {this.props.age} years old.</p>
       <p key="i2" >{this.props.children}</p>
-      <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}></input>
+      <input
+        key="i3"
+        // ref={(inputEl) => {this.inputElement = inputEl}}
+        ref={this.inputElementRef}
+        type="text"
+        onChange={this.props.changed}
+        value={this.props.name} />
     </Auxillary>
   }
 }
