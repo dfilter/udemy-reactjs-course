bc890f5de0e7c3796c189a5e7530e3b946af3dd1
dfilter <dfilter@evertz.com>
2020-08-17 11:29:15 -0400

    102. Rendering Adjacent JSX Elements

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 
+import Auxillary from '../../../hoc/Auxiliary'
 import classes from './Person.css'
 
+/**
+ * Since in jsx render can only return one expression we have one of two choices:
+ * 1. If a wrapping element is not needed for structure or styling an array of jsx can be returned instead.
+ * Don't forget the commas though!
+ * 2. Auxillary can be used here as well as another way to wrap adjacent jsx. This component doesn't do anything but let us 
+ * have multiple jsx elements next to one another.
+ */
 class Person extends Component {
 
   render() {
     console.log('[Person.js] rendering...')
 
-    return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>I'm {this.props.name}, and am {this.props.age} years old.</p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-      </div>
-    )
+    return <Auxillary>
+      <p key="i1" onClick={this.props.click}>I'm {this.props.name}, and am {this.props.age} years old.</p>
+      <p key="i2" >{this.props.children}</p>
+      <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}></input>
+    </Auxillary>
   }
 }
 
---
+++ react-complete-guide/src/hoc/Auxiliary.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+/**
+ * Component simply wraps around passed child components
+ */
+const aux = (props) => props.children
+
+export default aux
