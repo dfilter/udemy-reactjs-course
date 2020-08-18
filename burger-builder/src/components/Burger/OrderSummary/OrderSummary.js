7de7b6af3ab262845ba630e67e4ed09e6d34528d
dfilter <dfilter@evertz.com>
2020-08-18 11:01:55 -0400

    140. Adding a Custom Button Component

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,7 +17,9 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
-      <p>Continue to checkout.</p>
+      <p>Continue to checkout?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Auxiliary>
   )
 }
