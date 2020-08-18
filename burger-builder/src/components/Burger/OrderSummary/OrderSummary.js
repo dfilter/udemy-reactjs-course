5d14102ed60a37a2f98f1da3f844d92484ddfe6c
dfilter <dfilter@evertz.com>
2020-08-18 11:15:00 -0400

    142. Adding the Price to the Order Summary

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p><strong>Total Price: {props.price}</strong></p>
       <p>Continue to checkout?</p>
       <Button buttonType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button buttonType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
