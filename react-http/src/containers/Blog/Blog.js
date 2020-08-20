417b54628b1f55bc2cb6b4f1146100935c303355
dfilter <dfilter@evertz.com>
2020-08-20 14:21:59 -0400

    170. Handling Errors Locally

--- react-http/src/containers/Blog/Blog.js
+++ react-http/src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     postClickedHandler = (selectedPostId) => {
@@ -18,13 +19,16 @@ class Blog extends Component {
     }
 
     render() {
-        const posts = this.state.posts.map((post) => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postClickedHandler(post.id)} />
-        })
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map((post) => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postClickedHandler(post.id)} />
+            })
+        }
 
         return (
             <div>
@@ -52,6 +56,9 @@ class Blog extends Component {
                     }
                 })
                 this.setState({ posts: updatedPosts })
+            })
+            .catch((error) => {
+                this.setState({ error: true })
             })
     }
 }
